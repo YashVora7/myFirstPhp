78472fb2774bc03402cdbda49cf46693a77ae879
veertos <125968688+veertos@users.noreply.github.com>
2024-04-20 18:12:28 +0530

    php_tutor

--- connection.php
+++ connection.php
@@ -3,8 +3,9 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $database = "users";
 
-    $conn = mysqli_connect($servername, $username, $password);
+    $conn = mysqli_connect($servername, $username, $password, $database);
 
     if(!$conn){
         die("Connection Refused ".mysqli_connect_error());
---
+++ login.php
@@ -0,0 +1,91 @@
+<?php
+$showAlert = false;
+$showError = false;
+$login = false;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    require "connection.php";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password";
+    $result = mysqli_query($conn, $sql);
+    $num = mysqli_num_rows($result);
+    if ($num == 1) {
+        $login = true;
+        session_start();
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        header("location: welcome.php");
+    } else {
+        $showError = "Invalid Credentials";
+    }
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>login</title>
+</head>
+
+<body>
+    <?php
+
+    require "_nav.php";
+
+
+    if ($login) {
+
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+            <strong>Success!</strong> You login successfully.
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+            </button>
+        </div>";
+    }
+    if ($showError) {
+
+        echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+            <strong>Error!</strong> '$showError'.
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+            </button>
+        </div>";
+    }
+
+    ?>
+
+    <div class="container">
+        <form action="/login.php" action="post">
+            <div class="form-group">
+                <label for="username">UserName</label>
+                <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="text" class="form-control" id="password" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary">login</button>
+        </form>
+    </div>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
